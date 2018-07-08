ab56ce9be1997dd7201c7f0dbc9bdba3b8f9e835
Vince <vincentfiorilli@gmail.com>
2018-07-08 13:21:16 -0400

    gettin at it

---
+++ data.js
@@ -0,0 +1,55 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'additional_favorites_db'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+  //afterConnection()
+  //queryAllSongs()
+  createTable()
+});
+
+function createTable(){
+  connection.query('CREATE TABLE IF NOT EXISTS ice_cream_crud(size INT NOT NULL,flavor VARCHAR(50) NOT NULL)', function(err, res){
+    if (err) throw err;
+    console.log('table created');
+
+    connection.destroy()
+  })
+}
+
+function updateTable(){
+  connection.query('SELECT * FROM ice_cream_crud WHERE size = ?',
+    values: [3]),
+  function(err, res){
+    if (err) throw err;
+    console.log('table updated')
+    console.log(res)
+    connection.destroy()
+  })
+}
+
+function afterConnection(){
+  connection.query("SELECT * FROM favorite_foods", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+
+    connection.destroy()
+  });
+}
+
+function queryAllSongs(){
+  connection.query('SELECT * FROM favorite_songs', function(err, res){
+    console.log(res[1].song)
+    connection.destroy()
+  })
+}
