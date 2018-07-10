755a26861f1d4bcb9af7fea5865c3a3475c4094a
Vince <vincentfiorilli@gmail.com>
2018-07-10 19:14:10 -0400

    joins

--- top5000/top5000schema.sql
+++ top5000/top5000schema.sql
@@ -16,6 +16,44 @@ CREATE TABLE top5000 (
   PRIMARY KEY (position)
 );
 
+SELECT * FROM top_songsdb.top5000;
+
+USE top_songsdb;
+
+SELECT *
+FROM top5000
+WHERE artist = 'pink floyd';
+
+SELECT *
+FROM top5000
+WHERE year < 2000 AND year > 1955;
+
+CREATE TABLE top_albums (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT top5000.artist, top5000.year, top_albums.album, top5000.song
+FROM top5000
+INNER JOIN top_albums
+ON top5000.year = top_albums.year AND top_albums.artist = top5000.artist;
+
+SELECT * FROM top_albums;
+
+SELECT artist,
+COUNT(*) c
+FROM top5000
+GROUP BY artist
+HAVING c > 1; 
+
 SELECT * FROM top5000;
 
 SELECT * FROM top_songsdb.top5000;
@@ -30,4 +68,4 @@ SELECT artist,
 COUNT(*) c
 FROM top5000
 GROUP BY artist
-HAVING c > 1; 
+HAVING c > 1;
